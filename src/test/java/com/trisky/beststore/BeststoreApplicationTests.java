92f66101825818078ae3333775efa70e399fe314
Rasindu Sandeep <rasindu0823@gmail.com>
2024-09-05 19:44:24 +0530

    Basic Configuration done

---
+++ src/test/java/com/trisky/beststore/BeststoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trisky.beststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeststoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
